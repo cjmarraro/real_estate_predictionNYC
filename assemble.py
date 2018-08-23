2957f208c4df1470acd3bccd25a28e23eb9d7c06
Celia Marraro <cjmarraro@gmail.com>
2018-08-23 17:14:10 -0400

    commit

--- assemble.py
+++ assemble.py
@@ -4,45 +4,39 @@ import settings
 import pandas as pd 
 
 
-with open(settings.HEADERS_PATH) as header:
-    headerQ = header.readline().split(',')
+# with open(settings.HEADERS_PATH) as header:
+#     headerQ = header.readline().split(',')
 
 
 HEADERS = {
     "RepeatSales": [
-        "sales",
-        "MoM",
-        "YoY"
+       
     ],
 
     "Quarterly": [
-     input(headerQ)
-    ]
+    
+    ],
                  
     }
 
 SELECT = {
     "RepeatSales": HEADERS["RepeatSales"],
-    "Quarterly": headerQ   
+    "Quarterly": HEADERS["Quarterly"]  
     }
 
 def concatenate(prefix="RepeatSales"):
-    files = os.listdir(settings.DATA_DIR)
+    allFiles = os.listdir(settings.DATA_DIR)
     full = []
-    for f in files:
-        if not f.startswith(prefix):
+    for fName in allFiles:
+        if not fName.startswith(prefix):
             continue
         
-        if prefix is not "Quarterly":
-            data = pd.read_csv(os.path.join(settings.DATA_DIR, f), sep=',', header=None, names=HEADERS[prefix], index_col=False)
-        else:
-            data = pd.read_csv(os.path.join(settings.DATA_DIR, f), sep=',', header=None, names=headerQ, index_col=False)
-        
-        data = data[SELECT[prefix]]
+        data = pd.read_csv(os.path.join(settings.DATA_DIR, fName), sep=',', header=0, index_col=False)
+        # data = data[SELECT[prefix]]
         full.append(data)
 
     full = pd.concat(full, axis=0)
-    full.to_csv(os.path.join(settings.PROCESSED_DIR, "{}.txt".format(prefix)), header=SELECT[prefix], sep=",", index=False)
+    full.to_csv(os.path.join(settings.PROCESSED_DIR, "{}.txt".format(prefix)), sep=",", index=False)
 
 if __name__ == "__main__":
     concatenate("RepeatSales")
